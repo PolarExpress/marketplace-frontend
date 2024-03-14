880a2387e0300505e2a3a9baf12a25819f18dad0
zebby09 <z.n.h.josephusjitta@students.uu.nl>
2024-03-14 16:34:05 +0100

    Fix requested changes

--- src/pages/AddOnPage.tsx
+++ src/pages/AddOnPage.tsx
@@ -26,7 +26,7 @@ const AddOnPage = () => {
   );
 
   // Render simple error page when URL param not found in state
-  if (!thisAddOn) {
+  if (thisAddOn === undefined) {
     return <div data-testid="addon-not-found">Add-on not found</div>;
   }
 
