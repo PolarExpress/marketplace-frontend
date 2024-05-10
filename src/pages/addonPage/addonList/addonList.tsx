5264f621d65d23f3cb9b29b78edf470d68735af4
zebby09 <z.n.h.josephusjitta@students.uu.nl>
2024-05-10 15:15:10 +0200

    Use pagination component

--- src/pages/addonPage/addonList/addonList.tsx
+++ src/pages/addonPage/addonList/addonList.tsx
@@ -7,25 +7,30 @@
  */
 
 import { LoadingSpinner, RTKError } from "@polarexpress/components";
+import ReactPaginate from "react-paginate";
 import { type RootState, useAppSelector } from "@polarexpress/dataAccess/store";
 import { type Addon, AddonCategory } from "@polarexpress/types/addon";
 import AddonCard from "./addonCard";
 import { useGetAddonsQuery, useLazyGetAddonsQuery } from "./addonApi";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 
+/**
+ * Component that renders a grid of add-on cards with pagination.
+ *
+ * @returns The rendered AddonList component.
+ */
 const AddonList = () => {
-  // Get the current search term from the state
   const { searchTerm } = useAppSelector((state: RootState) => state.addons);
 
-  // Use the RTK Query hook to retrieve addons from the backend
+  const [currentPage, setCurrentPage] = useState(0);
+
   const {
     data: allAddOns,
     error: allError,
     isLoading: allLoading
   } = useGetAddonsQuery({
     category: AddonCategory.VISUALISATION,
-    // Temporary values
-    page: 0
+    page: currentPage
   });
 
   const [
@@ -33,9 +38,18 @@ const AddonList = () => {
     { data: filteredAddOns, error: filterError, isLoading: filterLoading }
   ] = useLazyGetAddonsQuery();
 
+  /**
+   * Handles the page click event in the pagination component.
+   *
+   * @param data The object containing the selected page index.
+   */
+  const handlePageClick = (data: { selected: number }) => {
+    setCurrentPage(data.selected);
+  };
+
   useEffect(() => {
-    trigger({ searchTerm });
-  }, [searchTerm, trigger]);
+    trigger({ page: currentPage, searchTerm });
+  }, [searchTerm, trigger, currentPage]);
 
   if (allLoading || filterLoading)
     return (
@@ -49,22 +63,39 @@ const AddonList = () => {
 
   const addOnsToRender = searchTerm ? filteredAddOns : allAddOns;
 
-  // Data might still be undefined
   if (addOnsToRender) {
-    // Check if searchTerm is present and no add-ons match the search
-    if (
-      searchTerm.length > 0 &&
-      filteredAddOns &&
-      filteredAddOns.length === 0
-    ) {
-      return <div>No Add-ons found with the given search term</div>;
+    if (addOnsToRender.addons.length === 0) {
+      return (
+        <p className="flex w-full justify-center">
+          No Add-ons found with the given search term
+        </p>
+      );
     }
 
     return (
-      <div className="relative flex w-full flex-wrap justify-center gap-4">
-        {addOnsToRender.map((addOn: Addon) => (
-          <AddonCard addOn={addOn} key={addOn._id} />
-        ))}
+      <div className="flex w-full flex-col items-center">
+        <div className="flex flex-wrap justify-center gap-4">
+          {addOnsToRender.addons.map((addOn: Addon) => (
+            <AddonCard addOn={addOn} key={addOn._id} />
+          ))}
+        </div>
+        <ReactPaginate
+          /* eslint-disable perfectionist/sort-jsx-props -- Alphabetic ordering would be illogical  */
+          breakLabel="..."
+          nextLabel=">"
+          previousLabel="<"
+          pageCount={addOnsToRender.totalPages}
+          pageRangeDisplayed={3}
+          onPageChange={handlePageClick}
+          breakClassName="items-center px-5 py-2 text-sm font-semibold text-black ring-1 ring-inset ring-gray-300 hover:bg-white inline-flex"
+          containerClassName="flex mt-5"
+          pageClassName="items-center px-5 py-2 text-sm font-semibold text-black ring-1 ring-inset ring-gray-300 hover:bg-white inline-flex"
+          previousLinkClassName="inline-flex items-center rounded-l-lg border-2 border-orange-400 px-4 py-2 text-sm font-medium text-black hover:bg-orange-400 hover:shadow-md hover:text-white"
+          nextLinkClassName="inline-flex items-center rounded-r-lg border-2 border-orange-400 px-4 py-2 text-sm font-medium text-black hover:bg-orange-400 hover:shadow-md hover:text-white"
+          activeClassName="bg-orange-400 text-gray-100 pointer-events-none"
+          disabledLinkClassName="hidden"
+          /* eslint-disable perfectionist/sort-jsx-props */
+        />
       </div>
     );
   }
