8acfd168e82aa988ab2b929d6a076e4f13f91924
Tjalle-S <91267211+Tjalle-S@users.noreply.github.com>
2024-05-31 11:36:48 +0200

    fix: correct title and icon

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -7,17 +7,20 @@
  */
 
 import SearchBar from "./searchBar";
+import GpLogo from "../../assets/gp-logo.svg";
 
 /**
  * Renders the main header area of the application, including a search bar.
  */
 const Header = () => {
   return (
-    <div
-      className="relative flex justify-center border-b bg-gray-50 px-6 py-3 shadow-black hover:z-50 hover:shadow-md"
+    <nav
+      className="flex h-12 w-full flex-row items-center gap-2 px-4 md:gap-3 lg:gap-4"
+      // className="relative flex justify-center items-center border-b bg-gray-50 px-6 py-3 shadow-black hover:z-50 hover:shadow-md"
       data-testid="header">
+      <img className="h-7" src={GpLogo} />
       <SearchBar />
-    </div>
+    </nav>
   );
 };
 
