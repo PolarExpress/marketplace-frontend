13d5546b57260c0c5bd0c5c87f3331f26e0dd239
Tjalle-S <91267211+Tjalle-S@users.noreply.github.com>
2024-06-01 21:52:34 +0200

    fix: reset on going to home

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -8,22 +8,20 @@
 
 import SearchBar from "./searchBar";
 import GpLogo from "../../assets/gp-logo.svg";
-import { Link } from "react-router-dom";
 
 /**
  * Renders the main header area of the application, including a search bar.
  */
-const Header = () => {
-  return (
-    <nav
-      className="relative flex items-center justify-center border-b bg-gray-50 px-6 py-3 shadow-black hover:z-50 hover:shadow-md"
-      data-testid="header">
-      <Link className="absolute left-0 mx-3" to="/">
-        <img className="h-8" src={GpLogo} />
-      </Link>
-      <SearchBar />
-    </nav>
-  );
-};
+const Header = () => (
+  <nav
+    className="relative flex items-center justify-center border-b bg-gray-50 px-6 py-3 shadow-black hover:z-50 hover:shadow-md"
+    data-testid="header">
+    <a className="absolute left-0 mx-3" href="/">
+      {/* It is probably better to just do a reload, instead of manyally resetting everything. */}
+      <img className="h-8" src={GpLogo} />
+    </a>
+    <SearchBar />
+  </nav>
+);
 
 export default Header;
