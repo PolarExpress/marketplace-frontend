1441572d23b57da35e1d487b932ace3643d2e1b1
Tjalle-S <91267211+Tjalle-S@users.noreply.github.com>
2024-06-01 21:39:37 +0200

    fix: icon link, positioning

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -8,6 +8,7 @@
 
 import SearchBar from "./searchBar";
 import GpLogo from "../../assets/gp-logo.svg";
+import { Link } from "react-router-dom";
 
 /**
  * Renders the main header area of the application, including a search bar.
@@ -15,10 +16,11 @@ import GpLogo from "../../assets/gp-logo.svg";
 const Header = () => {
   return (
     <nav
-      className="flex h-12 w-full flex-row items-center gap-2 px-4 md:gap-3 lg:gap-4"
-      // className="relative flex justify-center items-center border-b bg-gray-50 px-6 py-3 shadow-black hover:z-50 hover:shadow-md"
+      className="relative flex items-center justify-center border-b bg-gray-50 px-6 py-3 shadow-black hover:z-50 hover:shadow-md"
       data-testid="header">
-      <img className="h-7" src={GpLogo} />
+      <Link className="absolute left-0 mx-3" to="/">
+        <img className="h-8" src={GpLogo} />
+      </Link>
       <SearchBar />
     </nav>
   );
