15f25c52c2792e91ff6b6210dce652c1b174a7e1
Tjalle-S <91267211+Tjalle-S@users.noreply.github.com>
2024-06-03 13:38:04 +0200

    fix: correct title and icon (#77)
    * fix: correct title and icon
    
    * fix: icon link, positioning
    
    * fix: reset on going to home

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -7,18 +7,21 @@
  */
 
 import SearchBar from "./searchBar";
+import GpLogo from "../../assets/gp-logo.svg";
 
 /**
  * Renders the main header area of the application, including a search bar.
  */
-const Header = () => {
-  return (
-    <div
-      className="relative flex justify-center border-b bg-gray-50 px-6 py-3 shadow-black hover:z-50 hover:shadow-md"
-      data-testid="header">
-      <SearchBar />
-    </div>
-  );
-};
+const Header = () => (
+  <nav
+    className="relative flex items-center justify-center border-b bg-gray-50 px-6 py-3 shadow-black hover:z-50 hover:shadow-md"
+    data-testid="header">
+    <a className="absolute left-0 mx-3" href="/">
+      {/* It is probably better to just do a reload, instead of manyally resetting everything. */}
+      <img className="h-8" src={GpLogo} />
+    </a>
+    <SearchBar />
+  </nav>
+);
 
 export default Header;
